83ba1a7b617787de2504b22486d86736d22344e9
tobibako45 <tobibako45@gmail.com>
2019-02-06 19:51:54 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ancient-earth-43866.herokuapp.com'
+  host = 'still-beyond-14540.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
